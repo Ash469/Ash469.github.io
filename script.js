2073655f4f5390c2938275bc1650a4a0c604e070
ash469 <117721052+Ash469@users.noreply.github.com>
2024-05-18 15:27:19 +0530

    script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const buttons = {
          <h3>${item.date}</h3>
        </div>
        <div class="headline" style="display: flex; justify-content: center; color=green;">
-         <h2>${item.headline}</h2>
+         <h3>${item.headline}</h3>
        </div>
        <p>${item.content}</p>
      </content>
